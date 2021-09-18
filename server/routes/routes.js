58419d5dbe77781ae78081b6e12a7d0e873b61bd
hrathnapala <hrathnapala@gmail.com>
2021-09-18 10:50:49 +0530

    Modified route

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  res.send(`Server running on ${process.env.PORT || 3000}`);
+  res.send(`Running`);
 });
 
 module.exports = router;
