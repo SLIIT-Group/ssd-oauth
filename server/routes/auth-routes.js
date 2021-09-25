130ad2ecce86b00e7c5152d0c2062d77ed4d9750
Imal Kumarage <imalkrulez@gmail.com>
2021-09-25 11:10:19 +0530

    Added auth routes

---
+++ server/routes/auth-routes.js
@@ -0,0 +1,20 @@
+const router = require("express").Router();
+
+//auth login
+router.get("/login", (req, res) => {
+  res.send("Login Page");
+});
+
+//auth logout
+router.get("/logout", (res, req) => {
+  //Handle with passport
+  res.send("Logging out");
+});
+
+//auth with google
+router.get("/google", (req, res) => {
+  //handle with passport
+  res.send("Logging in with Google");
+});
+
+module.exports = router;
