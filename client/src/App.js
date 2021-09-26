267b744761b4af398217b59e3c90e68c1f7af161
thanujachamika <thanujachamikatc@gmail.com>
2021-09-27 02:15:04 +0530

    app.js file reformatted with prettier

--- client/src/App.js
+++ client/src/App.js
@@ -1,32 +1,32 @@
-import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import {BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom';
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import "./App.css";
 import ButtonRow from "./components/HomePage/buttonRow";
-import Footer from  "./components/Footer/footer";
+import Footer from "./components/Footer/footer";
 import Login from "./components/Login/login";
-import Gallery from "./components/Gallery/gallery"
+import Gallery from "./components/Gallery/gallery";
 import Navbar from "./components/Navbar/navbar";
 import Upload from "./components/Upload/upload";
 
 function App() {
   return (
-      <Router>
-          <div className="ssd-main-class">
-              <Navbar/>
-              <br/>
-              <Switch>
-                  <Route exact path = '/' component = {ButtonRow} />
-                  <Route exact path = '/upload' component={Upload} />
-                  <Route exact path = '/login' component = {Login} />
-                  <Route exact path = '/gallery' component={Gallery} />
-              </Switch>
+    <Router>
+      <div className="ssd-main-class">
+        <Navbar />
+        <br />
+        <Switch>
+          <Route exact path="/" component={ButtonRow} />
+          <Route exact path="/upload" component={Upload} />
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/gallery" component={Gallery} />
+        </Switch>
 
-                  <Footer></Footer>
+        <Footer></Footer>
 
-          {/*</div>*/}
-          </div>
-      </Router>
+        {/*</div>*/}
+      </div>
+    </Router>
   );
 }
 
