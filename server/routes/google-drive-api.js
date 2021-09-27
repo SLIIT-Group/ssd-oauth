59691c4440d2edd326f931a7fb7cee9b1efd9e3b
IT18111552 Weerakeshara T.N.N.D <thnuwanga@gmail.com>
2021-09-27 23:03:55 +0530

    be/ comments removed

--- server/routes/google-drive-api.js
+++ server/routes/google-drive-api.js
@@ -50,36 +50,6 @@ router.post("/getUserInfo", (req, res) => {
   });
 });
 
-/*
-router.post("/readDrive", (req, res) => {
-    if (req.body.token == null) return res.status(400).send("Token not found");
-    oAuth2Client.setCredentials(req.body.token);
-    const drive = google.drive({ version: "v3", auth: oAuth2Client });
-    drive.files.list(
-        {
-            pageSize: 10,
-        },
-        (err, response) => {
-            if (err) {
-                console.log("The API returned an error: " + err);
-                return res.status(400).send(err);
-            }
-            const files = response.data.files;
-            if (files.length) {
-                console.log("Files:");
-                files.map((file) => {
-                    console.log(`${file.name} (${file.id})`);
-                });
-            } else {
-                console.log("No files found.");
-            }
-            res.send(files);
-        }
-    );
-});
-*/
-
-
 router.post("/readDrive", async (req, res) => {
   try {
   if (req.body.token == null) return res.status(400).send("Token not found");
@@ -137,26 +107,6 @@ router.post("/readDrive", async (req, res) => {
     console.log(error.message);
   }
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 router.post("/fileUpload", (req, res) => {
   var form = new formidable.IncomingForm();
